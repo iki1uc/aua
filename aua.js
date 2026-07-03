3a2e81437d1825efa5a492fc9e810f4786a35feb
iki1uc <entertrainer@web.de>
2026-07-03 23:00:01 +0200

    Update aua.js

--- aua.js
+++ aua.js
@@ -1,1 +1,19 @@
+export const AUA = (measure) => {
+  if (measure > 0) {
+    return {
+      hit: true,
+      block: true,
+      save: true,
+      tmp: "0xA1",
+      msg: "AUA: Maß überschritten (1x)"
+    };
+  }
 
+  return {
+    hit: false,
+    block: false,
+    save: false,
+    tmp: "0x00",
+    msg: "AUA: sauber"
+  };
+};
